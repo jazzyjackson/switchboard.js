96e36bd2690681fc93ba875b2032344195e3cc0d
Colten Jackson <Colten Jackson>
2017-07-15 03:25:16 -0400

    Reworked indentation

--- switchboard.js
+++ switchboard.js
@@ -4,19 +4,38 @@ var http = require('http')
 var exec = require('child_process').exec
 var server = http.createServer((request,response) => ({
     'GET': () => fs.createReadStream(request.url.slice(1).split('?')[0] || 'index.html')
-                   .on('error', err => { response.writeHead(500); response.end( JSON.stringify(err)) })
-                   .on('open', () => console.log(request.url))
-                   .pipe(response),
-    'POST': () => exec(decodeURIComponent(request.url.split('/').slice(-1)), {
-                      cwd: process.cwd() + request.url.split('/').slice(0,-1).join('/')
-                  })
-                 .on('error', err => { response.writeHead(500); response.end( JSON.stringify(err)) })
-                 .on('exit', ()=>console.log('POST',request.url))
-                 .stdio.forEach(io => io.pipe(response)),
+                    .on('open', () => {
+                        
+                    })
+                    .on('error', err => { 
+                        response.writeHead(500); 
+                        response.end(JSON.stringify(err)) 
+                    })
+                    .pipe(response),
+
     'PUT': () => request.pipe(fs.createWriteStream('.' + request.url, 'utf8'))
-                        .on('finish', () => { response.writeHead(201); response.end() })
-                        .on('error', err => { response.writeHead(500); response.end( JSON.stringify(err)) }),
-    'DELETE': () => fs.unlink('.' + request.url, err => { response.writeHead( err ? 500 : 204); response.end(JSON.stringify(err))})
+                    .on('finish', () => { 
+                        response.writeHead(201); 
+                        response.end() 
+                    })
+                    .on('error', err => { 
+                        response.writeHead(500); 
+                        response.end(JSON.stringify(err)) 
+                    }),
+
+    'POST': () => exec(decodeURIComponent(request.url.split('/').slice(-1)), {
+                        cwd: process.cwd() + request.url.split('/').slice(0,-1).join('/')
+                    })
+                    .on('error', err => { 
+                        response.writeHead(500); 
+                        response.end(JSON.stringify(err)) 
+                    })
+                    .stdio.forEach(io => io.pipe(response)),
+    
+    'DELETE': () => fs.unlink('.' + request.url, err => { 
+                        response.writeHead( err ? 500 : 204); 
+                        response.end(JSON.stringify(err))
+                    })
 })[request.method]()).listen(process.argv[2]).on('listening', () => {
     var webaddress = 'http://' + getLocalIP() + ':' + server.address().port
     process.argv[2] ? console.log('running at', webaddress)
